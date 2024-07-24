b68da3657cb40231576d0feeb52c2afd11889146
neeltheninja <neeltheninja@gmail.com>
2024-07-24 22:16:11 +0530

    add support for fp16 by converting all tensors to the same dtype as q_target

--- attention_couple.py
+++ attention_couple.py
@@ -5,7 +5,6 @@ import comfy
 from comfy.ldm.modules.attention import optimized_attention
 
 def get_masks_from_q(masks, q, original_shape):
-
     if original_shape[2] * original_shape[3] == q.shape[1]:
         down_sample_rate = 1
     elif (original_shape[2] // 2) * (original_shape[3] // 2) == q.shape[1]:
@@ -17,12 +16,12 @@ def get_masks_from_q(masks, q, original_shape):
 
     ret_masks = []
     for mask in masks:
-        if isinstance(mask,torch.Tensor):
+        if isinstance(mask, torch.Tensor):
             size = (original_shape[2] // down_sample_rate, original_shape[3] // down_sample_rate)
             mask_downsample = F.interpolate(mask.unsqueeze(0), size=size, mode="nearest")
             mask_downsample = mask_downsample.view(1,-1, 1).repeat(q.shape[0], 1, q.shape[2])
             ret_masks.append(mask_downsample)
-        else: # coupling処理なしの場合
+        else:  # coupling処理なしの場合
             ret_masks.append(torch.ones_like(q))
     
     ret_masks = torch.cat(ret_masks, dim=0)
@@ -54,7 +53,7 @@ class AttentionCouple:
 
     def attention_couple(self, model, positive, negative, mode):
         if mode == "Latent":
-            return (model, positive, negative) # latent coupleの場合は何もしない
+            return (model, positive, negative)  # latent coupleの場合は何もしない
         
         self.negative_positive_masks = []
         self.negative_positive_conds = []
@@ -71,10 +70,10 @@ class AttentionCouple:
             conditions_conds = []
             if len(conditions) != 1:
                 mask_norm = torch.stack([cond[1]["mask"].to(device, dtype=dtype) * cond[1]["mask_strength"] for cond in conditions])
-                mask_norm = mask_norm / mask_norm.sum(dim=0) # 合計が1になるように正規化(他が0の場合mask_strengthの効果がなくなる)
+                mask_norm = mask_norm / mask_norm.sum(dim=0)  # 合計が1になるように正規化(他が0の場合mask_strengthの効果がなくなる)
                 conditions_masks.extend([mask_norm[i] for i in range(mask_norm.shape[0])])
                 conditions_conds.extend([cond[0].to(device, dtype=dtype) for cond in conditions])
-                del conditions[0][1]["mask"] # latent coupleの無効化のため
+                del conditions[0][1]["mask"]  # latent coupleの無効化のため
                 del conditions[0][1]["mask_strength"]
             else:
                 conditions_masks = [False]
@@ -86,32 +85,31 @@ class AttentionCouple:
         new_model = model.clone()
         self.sdxl = hasattr(new_model.model.diffusion_model, "label_emb")
         if not self.sdxl:
-            for id in [1,2,4,5,7,8]: # id of input_blocks that have cross attention
+            for id in [1,2,4,5,7,8]:  # id of input_blocks that have cross attention
                 set_model_patch_replace(new_model, self.make_patch(new_model.model.diffusion_model.input_blocks[id][1].transformer_blocks[0].attn2), ("input", id))
             set_model_patch_replace(new_model, self.make_patch(new_model.model.diffusion_model.middle_block[1].transformer_blocks[0].attn2), ("middle", 0))
-            for id in [3,4,5,6,7,8,9,10,11]: # id of output_blocks that have cross attention
+            for id in [3,4,5,6,7,8,9,10,11]:  # id of output_blocks that have cross attention
                 set_model_patch_replace(new_model, self.make_patch(new_model.model.diffusion_model.output_blocks[id][1].transformer_blocks[0].attn2), ("output", id))
         else:
-            for id in [4,5,7,8]: # id of input_blocks that have cross attention
-                block_indices = range(2) if id in [4, 5] else range(10) # transformer_depth
+            for id in [4,5,7,8]:  # id of input_blocks that have cross attention
+                block_indices = range(2) if id in [4, 5] else range(10)  # transformer_depth
                 for index in block_indices:
                     set_model_patch_replace(new_model, self.make_patch(new_model.model.diffusion_model.input_blocks[id][1].transformer_blocks[index].attn2), ("input", id, index))
             for index in range(10):
                 set_model_patch_replace(new_model, self.make_patch(new_model.model.diffusion_model.middle_block[1].transformer_blocks[index].attn2), ("middle", id, index))
-            for id in range(6): # id of output_blocks that have cross attention
-                block_indices = range(2) if id in [3, 4, 5] else range(10) # transformer_depth
+            for id in range(6):  # id of output_blocks that have cross attention
+                block_indices = range(2) if id in [3, 4, 5] else range(10)  # transformer_depth
                 for index in block_indices:
                     set_model_patch_replace(new_model, self.make_patch(new_model.model.diffusion_model.output_blocks[id][1].transformer_blocks[index].attn2), ("output", id, index))
         
-        return (new_model, [new_positive[0]], [new_negative[0]]) # pool outputは・・・後回し
+        return (new_model, [new_positive[0]], [new_negative[0]])  # pool outputは・・・後回し
     
     def make_patch(self, module):
         def patch(q, k, v, extra_options):
-            
-            len_neg, len_pos = self.conditioning_length # negative, positiveの長さ
-            cond_or_uncond = extra_options["cond_or_uncond"] # 0: cond, 1: uncond
+            len_neg, len_pos = self.conditioning_length  # negative, positiveの長さ
+            cond_or_uncond = extra_options["cond_or_uncond"]  # 0: cond, 1: uncond
             q_list = q.chunk(len(cond_or_uncond), dim=0)
-            b = q_list[0].shape[0] # batch_size
+            b = q_list[0].shape[0]  # batch_size
             
             masks_uncond = get_masks_from_q(self.negative_positive_masks[0], q_list[0], extra_options["original_shape"])
             masks_cond = get_masks_from_q(self.negative_positive_masks[1], q_list[0], extra_options["original_shape"])
@@ -140,6 +138,11 @@ class AttentionCouple:
                 q_target = q_list[i].repeat(length, 1, 1)
                 k = torch.cat([k[i].unsqueeze(0).repeat(b,1,1) for i in range(length)], dim=0)
                 v = torch.cat([v[i].unsqueeze(0).repeat(b,1,1) for i in range(length)], dim=0)
+
+                # Convert all tensors to the same dtype as q_target
+                k = k.to(dtype=q_target.dtype)
+                v = v.to(dtype=q_target.dtype)
+                masks = masks.to(dtype=q_target.dtype)
 
                 qkv = optimized_attention(q_target, k, v, extra_options["n_heads"])
                 qkv = qkv * masks
